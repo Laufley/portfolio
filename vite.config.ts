7a69802564d746a477b57799d1201ec1178d824b
Your Name <ibg.indira@gmail.com>
2025-10-20 13:45:30 +0100

    add vite.config.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
@@ -12,5 +13,5 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-  },``
+  },
 })
