f6bf0ae91ce4fc45148afb7659123b17ca4f9747
Your Name <ibg.indira@gmail.com>
2025-10-21 14:28:19 +0100

    turn projects js into ts

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,26 +1,31 @@
-import { Link } from 'react-router-dom'
-import './Projects.css'
+import React from 'react';
+import './Projects.css';
+import Carousel from './carousel'
 
-function Projects() {
-  const projects = [
-    { id: 1, title: 'Project 1', description: 'Description 1' },
-    { id: 2, title: 'Project 2', description: 'Description 2' },
-    { id: 3, title: 'Project 3', description: 'Description 3' },
-  ]
-
-  return (
-    <div className="projects">
-      <h1>My Projects</h1>
-      <div className="projects-grid">
-        {projects.map(project => (
-          <Link to={`/projects/${project.id}`} key={project.id} className="project-card">
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-          </Link>
-        ))}
-      </div>
-    </div>
-  )
+export interface ProjectType {
+  title: string;
+  projectImg: string;
+  github_link: string;
+  demo_link: string;
+  stack: string[];
+  nature: string;
+  duration: string;
+  info: string;
+  challenge: string;
+  solution: string;
+  features: string[];
 }
 
-export default Projects
+interface ProjectsProps {
+  projects: ProjectType[];
+}
+
+const Projects: React.FC<ProjectsProps> = ({ projects }) => {
+  return (
+    <div id="page-wrapper">
+      <Carousel projects={projects} />
+    </div>
+  );
+};
+
+export default Projects;
