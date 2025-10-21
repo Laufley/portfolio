b67468e89dc95c1f76ff89e2e28644165bfef0cd
Your Name <ibg.indira@gmail.com>
2025-10-21 20:21:48 +0100

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Home from './Containers/Home'
 import Projects from './Components/Projects'
 import About from './Components/About'
 import CV from './Components/CV'
-import ProjectByIdPage from './Components/ProjectByIdPage'
 import ErrorPage from './Components/ErrorPage'
 import './App.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Header from './Components/Header'
+import projectsData from './Data/projectsData';
 
 function App() {
   return (
@@ -16,9 +16,9 @@ function App() {
         <Header/>
         <main>
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/projects" element={<Projects projects={[]} />} />
-            <Route path="/projects/:id" element={<ProjectByIdPage />} />
+            <Route path="/projects" element={<Projects projects={projectsData} />} />
             <Route path="/about" element={<About />} />
             <Route path="/cv" element={<CV />} />
             <Route path="*" element={<ErrorPage />} />
