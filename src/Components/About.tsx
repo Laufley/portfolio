fff4f6c7c00f10d29cbfb9d71f9f2842f865a370
Your Name <ibg.indira@gmail.com>
2025-10-23 15:13:53 +0100

    add content

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -3,7 +3,7 @@ import './About.css';
 import hallelujah from './Assets/hallelujah.mp3';
 
 function About() {
-  const [flipped, setFlipped] = useState([false, false, false, false]);
+  const [flipped, setFlipped] = useState([false, false, false, false, false]); // Added one more state for mini-card
   const audio = new Audio(hallelujah);
 
   const handleFlip = (index: number) => {
@@ -31,7 +31,7 @@ function About() {
       front: "Life in Barcelona",
       back: [
         "I grew up in a <strong>very impoverished</strong> neighborhood outside BCN.",
-        "An IT career was unheard of (there) by that time. But I loved science!",
+        "An IT career was unheard of (there) by that time.",
         "<strong>High school:</strong> I pursued Bachillerato Científico.",
         "<strong>Uni:</strong> Worked and studied hard to obtain grants for Clinical Psychology.",
         "<strong>The turn point:</strong> I graduated, worked in a hostpital, and discovered how tainted the field was with pharma. I could not morally continue that path.",
@@ -41,33 +41,39 @@ function About() {
     {
       front: "Life in Edinburgh",
       back: [
-        "<strong><u>Bad news:</u></strong> Qualified for the doctorate, but the salary would be too low.",
-        "I worked as a <strong>waitress in a kebab shop</strong>. I had to learn English anyway.",
-        "I leveled up to <strong>Customer Service Agent</strong> to boost my English.",
-        "I got promoted to <strong>Social Media Engagement Coord. </strong> - First client meetings!",
-        "I wanted to grow. By then, I knew about IT as a career, but I didn't have the education. I wish I had. So I decided to watch as an <strong>IT Recruiter</strong>.",
+        "<strong><u>Sour news:</u></strong> Qualified for the doctorate, but the salary would be too low.",
+        "I started as a <strong>waitress in a kebab shop</strong>. I had to learn English anyway.",
+        "I changed to <strong>Customer Service Agent</strong> to boost my English.",
+        "I got promoted to <strong>Social Media Coord. </strong> - First client meetings!",
+        "I wanted to grow. By then, I knew about IT as a career, but I didn't have the education. I wish I had. But I touched the field as <strong>IT Recruiter</strong>.",
         "I got promoted to Senior. I also helped to design our website and logo! ;)",
-        "But through befriending devs, I got jealous of their skills.",
-        "So I did a bootcamp. Again, investing all my savings. It changed EVERYTHING",
-        "<strong><u>Good news:</u></strong> I officially became a <span class='sound-trigger' onclick='handleSoundPlay'><strong><u>Developer</u></strong></span>. Finally!",
+        "But I got jealous of the dev's skills and their power of creation. So I did a bootcamp. Again, investing all my savings. It changed EVERYTHING!",
+        "<strong><u>Good news:</u></strong> I officially became a <span class='sound-trigger' onclick='handleSoundPlay'><strong><u>Software Developer</u></strong></span>!",
       ]
     },
     {
       front: "Hobbies & Interests",
       back: [
-         "Building my (secret & private) app as a side-project.",
-         "Writing stories cooperatively & worldbuilding with others.",
-         "Playing tabletop rpgs (GURPS, D&D) with friends",
-         "Playing boardgames and videogames.",
-         "Reading - My fav author is Brandon Sanderson.",
-         "Painting Warhammer miniatures for tabletop games.",
-         "Hiking.",
-         "Traveling - I finally went to Japan!",
+         "<strong>Building</strong> my (secret) app as a side-project.",
+         "<strong>Writing</strong> stories cooperatively & <strong>worldbuilding</strong> with others.",
+         "<strong>Reading</strong> - My fav author is Brandon Sanderson.",
+         "<strong>Painting</strong> Warhammer miniatures for tabletop games.",
+         "<strong>Playing tabletop rpgs</strong>, boardgames and videogames.",
+         "<strong>Piano playing</strong> - not well, but I try.",
+         "<strong>Hiking.</strong>",
+         "<strong>Traveling</strong> - I finally went to Japan!",
       ]
     },
     {
-      front: "Flip me (?)",
-      back: "Peekaboo!"
+      front: "Present, Future & Learnings",
+      back: [
+        "<strong><span class='lesson-2'>Present:</span></strong> Have you noticed what do my hobbies have in common? Creation and problem solving. Yes, this is what drives me as a Developer.",
+        "<strong><span class='lesson-2'>Future:</span></strong> I'll keep enjoying front-end, back-end and AWS cloud. And I am  pursuing a Master's in Software Development to expand my knowledge.",
+        "<strong><span class='lesson-2'>Lesson 1:</strong> <span class='lesson-1'>Life is unpredictable. Embrace change.",
+        "<strong><span class='lesson-2'>Lesson 2:</strong> <span class='lesson-1'>The best gamble you can do, is against yourself.</span>",
+        "<strong><span class='lesson-2'>Lesson 3:</strong> <span class='lesson-1'>We can only make descisions with the information we have at that time... But you can always adjust later.</span>",
+        "<strong><span class='lesson-2'>Lesson 4:</strong> <span class='lesson-1'>When life gives you lemons... first check if <span class='lesson-2'> <i>lemons != null.</i></span>",
+      ]
     },
   ];
 
@@ -102,16 +108,22 @@ function About() {
             </div>
           </div>
         ))}
+        <div 
+          className={`mini-card ${flipped[4] ? 'flipped' : ''}`} 
+          onClick={() => handleFlip(4)}
+        >
+          <div className="mini-card-inner">
+            <div className="mini-card-front">
+              <h3>👀 ? </h3>
+            </div>
+            <div className="mini-card-back">
+              <h3>🍋🍋🍋</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default About;
-
-
-// the best gamble you can do, is against yourself
-// life is hard, but so are you
-// poverty chases you your whole life, the fact that I don't have an IT degree is a scar I will always carry. BUT hey, news! i'm now taking a master's in Software development so that will fix it a bit (?)
-// when life gives you lemons... ask yourself, what would indira do? and then do the opposite.
-// we can only make descisions with the information we have
