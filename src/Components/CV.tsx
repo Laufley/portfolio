63f78ad85e43e676300d43e62b94acba48ca3dcb
Your Name <ibg.indira@gmail.com>
2025-10-24 09:09:59 +0100

    fix about mobile

--- src/Components/CV.tsx
+++ src/Components/CV.tsx
@@ -1,10 +1,27 @@
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import CVFile from './Assets/my_cv.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
 function CV() {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = CVFile;
+    link.download = 'CV_Indira_Borras.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <div className="cv">
-      <h1>My CV</h1>
-      <p>Page under construction 🔨 </p>
+    <div className="cv-wrapper">
+      <FontAwesomeIcon icon={faDownload} />
+      <button onClick={handleDownload} className="download-button">
+        Download CV
+      </button>
     </div>
-  )
+  );
 }
 
-export default CV
+export default CV;
